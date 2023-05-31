f0f3849f7f79a9b80026e021108e163e15fb1aa1
IvanYou98 <yuanfanyou@gmail.com>
2023-05-30 22:54:41 -0400

    add get mapping to the request handler

--- src/main/java/com/example/userapidemo/controller/TestController.java
+++ src/main/java/com/example/userapidemo/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.example.userapidemo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @GetMapping
     public String helloWorld() {
         return "Hello World";
     }
