7d5a7aca323ed233ec29e1a4a5d0ce1f347da051
IvanYou98 <yuanfanyou@gmail.com>
2023-05-30 22:53:02 -0400

    extend SpringBootServletInitializer in the runner class

--- src/main/java/com/example/userapidemo/UserApiDemoApplication.java
+++ src/main/java/com/example/userapidemo/UserApiDemoApplication.java
@@ -2,9 +2,10 @@ package com.example.userapidemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UserApiDemoApplication {
+public class UserApiDemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(UserApiDemoApplication.class, args);
